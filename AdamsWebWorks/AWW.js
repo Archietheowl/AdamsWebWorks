2972ef4f1609c628cebca1f8cc552f0c785a951a
ArchieTheOwl <104842675+Archietheowl@users.noreply.github.com>
2022-06-03 10:13:34 +0100

    Add files via upload

---
+++ AdamsWebWorks/AWW.js
@@ -0,0 +1,21 @@
+
+//***************Timeline Animation******************
+
+var $element=$('.each-event, .title');
+    var $window = $(window);
+    $window.on('scroll resize', check_for_fade);
+    $(window).trigger('scroll');
+    function check_for_fade(){
+        var window_height = $window.height()
+        $.each($element, function (event){
+            var $element = $(this);
+            var element_height = $element.outerHeight()-175; //adjusted to get elements to come in earlier.
+            var element_offset = $element.offset().top;
+            space = window_height - (element_height + element_offset - $(window).scrollTop());
+            if(space < 60){
+                $element.addClass("non-focus");
+            }else{
+                $element.removeClass("non-focus");
+            }
+        });
+    };        
